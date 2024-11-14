75da463df3f152e45e0d997a2a95b1a66ffbf3e8
Holger Seelig <holger.seelig@gmail.com>
2024-11-14 21:56:52 +0100

    Added types.

---
+++ dist/traverse.d.ts
@@ -0,0 +1,22 @@
+export = traverse;
+
+declare function traverse(X3D: X3D): typeof Traverse;
+
+class Traverse
+{
+   static readonly NONE: number;
+   static readonly EXTERNPROTO_DECLARATIONS: number;
+   static readonly PROTO_DECLARATIONS: number;
+   static readonly ROOT_NODES: number;
+   static readonly IMPORTED_NODES: number;
+   static readonly EXTERNPROTO_DECLARATION_SCENE: number;
+   static readonly PROTO_DECLARATION_BODY: number;
+   static readonly PROTOTYPE_INSTANCES: number;
+   static readonly INLINE_SCENE: number;
+   static readonly ALL: number;
+
+   /**
+    * Traverse object.
+    */
+   static *traverse (object: any, flags?: number = 0): Iterable <X3D .SFNode>;
+}
